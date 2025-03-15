8908ba8bb12860f4cb2cfaa2889888b0daaf480c
Eric <xyan0147@student.monash.edu>
2025-03-16 01:09:02 +0800

    Add half adder and full adder implementations in Verilog

---
+++ adders/full_adder.v
@@ -0,0 +1,9 @@
+`include "half_adder.v"
+`include "../logic-gates/others.v"
+
+module FullAdder(input a, b, cin, output sum, cout);
+    wire sum1, carry1, carry2;
+    HalfAdder ha1(a, b, sum1, carry1);
+    HalfAdder ha2(sum1, cin, sum, carry2);
+    Or or_gate(carry1, carry2, cout);
+endmodule
---
+++ adders/half_adder.v
@@ -0,0 +1,7 @@
+`include "../logic-gates/others.v"
+
+module HalfAdder(input a, b, output sum, carry);
+    // Instantiate XOR and AND gates
+    Xor xor_gate(a, b, sum);
+    And and_gate(a, b, carry);
+endmodule
